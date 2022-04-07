660afcfea7bc69ca891d7b52ee34acd27a40e6b9
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2022-04-08 06:32:52 +0900

    ユーザーIDを変更できるようにした

--- index.js
+++ index.js
@@ -1,47 +1,27 @@
-// ユースケース: Ajax通信
-
-/* 1.エントリーポイント
-console.log("index.js: loaded");
-
-// CSSセレクタを使ってDOMツリー中のh2要素を取得する
-const heading = document.querySelector("h2");
-// h2要素に含まれるテキストコンテンツを取得する
-const headingText = heading.textContent;
-
-// button要素を作成する
-const button = document.createElement("button");
-button.textContent = "Push Me";
-// body要素の子要素としてbuttonを挿入する
-document.body.appendChild(button);
-
-alert(headingText);
-*/
-
-
-// const userId = "akrmyuc";
-// fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`);
-// encodeURIComponent関数：/や%などURLとして特殊な意味を持つ文字列をただの文字列として扱えるようにエスケープする関数
-
-function main() {
-    fetchUserInfo("akrmyuc");
+async function main() {
+    try {
+        const userId = getUserId();
+        const userInfo = await fetchUserInfo(userId);
+        const view = createView(userInfo);
+        displayView(view);
+    } catch (error) {
+        console.error(`エラーが発生しました (${error})`);
+    }
 }
 
 function fetchUserInfo(userId) {
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
             if (!response.ok) {
-                console.error("エラーレスポンス", response);
+                return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
             } else {
-                return response.json().then(userInfo => {
-                    // HTMLの組み立て
-                    const view = createView(userInfo);
-                    // HTMLの挿入
-                    displayView(view);
-                });
+                return response.json();
             }
-        }).catch(error => {
-            console.error(error);
         });
+}
+
+function getUserId() {
+    return document.getElementById("userId").value;
 }
 
 function createView(userInfo) {
@@ -62,7 +42,6 @@ function displayView(view) {
     result.innerHTML = view;
 }
 
-// 特殊な記号に対するエスケープ処理
 function escapeSpecialChars(str) {
     return str
         .replace(/&/g, "&amp;")
@@ -82,4 +61,3 @@ function escapeHTML(strings, ...values) {
         }
     });
 }
-
